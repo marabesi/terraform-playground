948ed3c08b2c8f77fb17fd0df128694ad819454e
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-11-21 21:57:09 +0100

    chore: created aws-infra folder

---
+++ aws-infrastructure/main.tf
@@ -0,0 +1,20 @@
+terraform {
+ required_providers {
+   aws = {
+     version = "~> 3.53.0"
+   }
+ }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "frontend-app-playground"
+
+  tags = {
+    Name        = "bucket"
+    Environment = "frontend-app-playground"
+  }
+}
