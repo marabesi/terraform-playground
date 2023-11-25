076bd0dcf7d769411814a17e39ae23ff5668071d
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-11-25 16:15:22 +0100

    fix: access to the bucket

--- aws-infrastructure/s3-bucket/main.tf
+++ aws-infrastructure/s3-bucket/main.tf
@@ -7,7 +7,31 @@ resource "aws_s3_bucket" "webapp" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "webapp_ownership" {
+  for_each = var.environments
+  bucket = aws_s3_bucket.webapp[each.value].id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "webapp_access_block" {
+  for_each = var.environments
+  bucket = aws_s3_bucket.webapp[each.value].id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "webapp_public_read" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.webapp_ownership,
+    aws_s3_bucket_public_access_block.webapp_access_block,
+  ]
+
   for_each = var.environments
   bucket = aws_s3_bucket.webapp[each.value].id
   acl = "public-read"
