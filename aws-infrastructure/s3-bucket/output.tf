3e4ab31d95e1de5e30d1d009ba9b03c337c83fc0
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-11-25 18:53:23 +0100

    fix: output from s3

--- aws-infrastructure/s3-bucket/output.tf
+++ aws-infrastructure/s3-bucket/output.tf
@@ -1,5 +1,5 @@
 output "frontend_app_endpoint" {
   value = [
-    for environment in aws_s3_bucket.webapp : environment
+    for environment in aws_s3_bucket.webapp : environment.website_endpoint
   ]
 }
