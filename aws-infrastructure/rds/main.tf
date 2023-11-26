c6f59a97c8baab6511fdd30235723abd360a71db
marabesi <matheus.marabesi@gmail.com>
2023-11-26 19:42:57 +0100

    chore: rds public available and storage to 5gb

--- aws-infrastructure/rds/main.tf
+++ aws-infrastructure/rds/main.tf
@@ -1,8 +1,9 @@
 resource "aws_db_instance" "database" {
   for_each = var.environments
-  allocated_storage    = 10
+  allocated_storage    = 5
   # https://developer.hashicorp.com/terraform/language/functions/replace
   db_name              = "${each.value}${replace(var.project_name, "-", "")}"
+  identifier           = "${each.value}-${var.project_name}"
   engine               = "postgres"
   engine_version       = "14"
   instance_class       = "db.t3.micro"
@@ -10,4 +11,5 @@ resource "aws_db_instance" "database" {
   password             = var.rds_root_password
   skip_final_snapshot  = true
   deletion_protection = false
+  publicly_accessible = true
 }
