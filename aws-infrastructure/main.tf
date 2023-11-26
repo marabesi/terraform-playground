f0e5d4e6a0ba80ec7858cb39b698aca1f3e6fa57
marabesi <matheus.marabesi@gmail.com>
2023-11-26 17:50:46 +0100

    chore: enables rds

--- aws-infrastructure/main.tf
+++ aws-infrastructure/main.tf
@@ -30,10 +30,10 @@ provider "aws" {
 #  project_name = var.project_name
 #}
 
-#module "database" {
-#  source = "./rds"
-#  environments = var.environments
-#  project_name = var.project_name
-#  rds_root_username = var.rds_root_username
-#  rds_root_password = var.rds_root_password
-#}
+module "database" {
+  source = "./rds"
+  environments = var.environments
+  project_name = var.project_name
+  rds_root_username = var.rds_root_username
+  rds_root_password = var.rds_root_password
+}
